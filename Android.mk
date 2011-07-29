2ce46e757553e4b840e9320a3327294f8aed797e
Sebastian Schmidt <yath@yath.de>
2011-07-29 17:47:29 +0200

    Change tabs to spaces

--- Android.mk
+++ Android.mk
@@ -6,10 +6,10 @@ LOCAL_MODULE    := fuse
 
 LOCAL_SRC_FILES := $(addprefix lib/,cuse_lowlevel.c fuse.c fuse_kern_chan.c fuse_loop.c fuse_loop_mt.c fuse_lowlevel.c fuse_mt.c fuse_opt.c fuse_session.c fuse_signals.c helper.c mount.c mount_util.c ulockmgr.c)
 
-LOCAL_C_INCLUDES 		:= $(addprefix $(LOCAL_PATH)/,include android-include)
+LOCAL_C_INCLUDES        := $(addprefix $(LOCAL_PATH)/,include android-include)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
-LOCAL_CFLAGS    	:= -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26
+LOCAL_CFLAGS        := -D_FILE_OFFSET_BITS=64 -DFUSE_USE_VERSION=26
 LOCAL_EXPORT_CFLAGS := $(LOCAL_CFLAGS)
 
 LOCAL_LDLIBS := -Wl,--version-script=lib/fuse_versionscript
